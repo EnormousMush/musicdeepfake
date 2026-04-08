473cf6b1aee52a5e30eaf8c6b0fd448c20393ab6
Runbao Du <frankdurunbao@163.com>
2026-04-07 21:40:07 -0500

    code I used to determine what genres are in each sample size of FMA github download

---
+++ Part 1/snippets/fma_genre_determination.py
@@ -0,0 +1,13 @@
+import pandas as pd
+
+# Load with multi-level header
+tracks = pd.read_csv('tracks.csv', index_col=0, header=[0, 1])
+
+# Filter to "small" subset
+small = tracks[tracks[('set', 'subset')] == 'small']
+
+# List all genres in small
+print("Genres in 'small':")
+print(small[('track', 'genre_top')].value_counts())
+
+print(f"\nTotal tracks in 'small': {len(small)}")
